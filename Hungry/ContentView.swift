0605c37ee032c976d9062bf95bc63e744ec91035
Rodney Gainous Jr <rodney.gainous@gmail.com>
2024-07-03 13:51:34 -0700

    Initial Commit

---
+++ Hungry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hungry
+//
+//  Created by Rodney Gainous Jr on 7/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hungry/HungryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HungryApp.swift
+//  Hungry
+//
+//  Created by Rodney Gainous Jr on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HungryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
