8e00c03c833cc2ae3ec30886f3c9a208f27193c0
Rodney Gainous Jr <rodney.gainous@gmail.com>
2024-07-05 12:40:29 -0700

    Create DietaryFilter enum

---
+++ Hungry/DietaryFilter.swift
@@ -0,0 +1,16 @@
+//
+//  DietaryFilter.swift
+//  Hungry
+//
+//  Created by Rodney Gainous Jr on 7/5/24.
+//
+
+import Foundation
+
+enum DietaryFilter: String, CaseIterable {
+    case all = "All"
+    case vegetarian = "Vegetarian"
+    case vegan = "Vegan"
+    case glutenFree = "Gluten-Free"
+    case dairyFree = "Dairy-Free"
+}
